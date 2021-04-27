b02f574ab380c0cef3421a23bb86c95d989c3ab5
Lozan <lozan2312@gmail.com>
2021-04-27 23:21:36 +0300

    'update'

---
+++ mygit/src/component/general/card_number/card_number.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from '../button/button'
+
+
+class CardNumber extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div id={this.props.id}>
+        <div className="card-deck ml-4">
+        {this.props.json.map((item) => {
+          return(
+            (("icon" in item)==1)?
+              <div className="card">
+                <div className="card-body text-center">
+                  <span className="card-title">{item.number}</span>
+                  <p className="card-text mt-4">{item.text}</p>
+                  <a className="display-4" href={item.url}>
+                    <i className={item.icon}></i>
+                  </a>
+                </div>
+            </div>:
+            <div className="card">
+              <div className="card-body text-center">
+                <a href={this.props.href}>
+                  <span className="card-title">{item.number}</span>
+                  <p className="card-text mt-4">{item.text}</p>
+                </a>
+              </div>
+          </div>
+          )
+          })}
+        </div>
+      </div>
+    )}}
+
+export default CardNumber;
